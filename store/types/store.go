091357f979fd667308b59aab3fa51d247bb2bc4d
yihuang <huang@crypto.com>
2024-03-30 00:32:58 +0800

    Problem: nested cache store not efficient (#241)
    * Problem: nested cache store not efficient
    
    Solution:
    - introduce copy-on-write btree based cache store
    
    temp
    
    * changelog
    
    * rename
    
    * Update store/cachekv/store.go
    
    Signed-off-by: yihuang <huang@crypto.com>
    
    ---------
    
    Signed-off-by: yihuang <huang@crypto.com>

--- store/types/store.go
+++ store/types/store.go
@@ -346,6 +346,12 @@ func (cid CommitID) String() string {
 	return fmt.Sprintf("CommitID{%v:%X}", cid.Hash, cid.Version)
 }
 
+// BranchStore
+type BranchStore interface {
+	Clone() BranchStore
+	Restore(BranchStore)
+}
+
 //----------------------------------------
 // Store types
 
