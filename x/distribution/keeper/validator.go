03c0aff28919e51d7a76436814c09dac648d2310
Thomas Nguy <thomas.nguy@crypto.com>
2025-07-09 00:47:19 +0900

    Revert "Problem: Int overflow when sum up (#1622)"
    This reverts commit f67eec43305b5165be32bf707e4cc4d047af17bd.

--- x/distribution/keeper/validator.go
+++ x/distribution/keeper/validator.go
@@ -42,16 +42,6 @@ func (k Keeper) initializeValidator(ctx context.Context, val stakingtypes.Valida
 
 // increment validator period, returning the period just ended
 func (k Keeper) IncrementValidatorPeriod(ctx context.Context, val stakingtypes.ValidatorI) (uint64, error) {
-	defer func() {
-		if err := recover(); err != nil {
-			if str, ok := err.(string); ok && str == "Int overflow" {
-				k.Logger(ctx).Error("integer overflow occurred", "err", err)
-			} else {
-				panic(err)
-			}
-		}
-	}()
-
 	valBz, err := k.stakingKeeper.ValidatorAddressCodec().StringToBytes(val.GetOperator())
 	if err != nil {
 		return 0, err
