1d53ef4da959d89a79dbd83b42e6264e465eb4fa
Randy Ang <randy.ang@crypto.com>
2025-09-25 22:02:21 +0800

    fix integration test

--- tests/integration/staking/keeper/determinstic_test.go
+++ tests/integration/staking/keeper/determinstic_test.go
@@ -816,7 +816,7 @@ func TestGRPCRedelegations(t *testing.T) {
 		DstValidatorAddr: validator2,
 	}
 
-	testdata.DeterministicIterations(f.ctx, t, req, f.queryClient.Redelegations, 3920, false)
+	testdata.DeterministicIterations(f.ctx, t, req, f.queryClient.Redelegations, 3914, false)
 }
 
 func TestGRPCParams(t *testing.T) {
