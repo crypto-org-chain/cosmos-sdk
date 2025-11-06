5c5e8a99a1c43a5b27489cc57278cb23b1b3f660
Randy Ang <randy.ang@crypto.com>
2025-11-06 12:55:41 +0800

    fix comment

--- x/staking/module.go
+++ x/staking/module.go
@@ -230,7 +230,7 @@ func ProvideModule(in ModuleInputs) ModuleOutputs {
 	k := keeper.NewKeeper(
 		in.Cdc,
 		in.StoreService,
-		in.CacheStoreService, // Memory store for cache (optional, can be nil)
+		in.CacheStoreService, // Memory store for cache
 		in.AccountKeeper,
 		in.BankKeeper,
 		authority.String(),
