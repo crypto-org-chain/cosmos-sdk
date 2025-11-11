1a1d36303f125d36078c00fc7f94f642799e0eb1
Randy Ang <randy.ang@crypto.com>
2025-11-11 15:39:12 +0800

    Revert "fix import"
    This reverts commit 2b94181423751e664eb7fad31cc563ea2c4d4b6b.

--- x/staking/cache/cache_test.go
+++ x/staking/cache/cache_test.go
@@ -14,7 +14,6 @@ import (
 	"github.com/cosmos/cosmos-sdk/testutil"
 
 	"cosmossdk.io/log"
-	"github.com/cosmos/cosmos-sdk/baseapp"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	moduletestutil "github.com/cosmos/cosmos-sdk/types/module/testutil"
 	"github.com/cosmos/cosmos-sdk/x/staking"
