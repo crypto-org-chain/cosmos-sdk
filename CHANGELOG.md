b1c3b2cd5376d349d96782b32ff14b5ad975b1af
jay.tseng <jay.tseng@crypto.com>
2026-07-10 13:10:56 -0400

    fix(server): lower default gRPC MaxSendMsgSize from ~2GB to 10MB
    DefaultGRPCMaxSendMsgSize was math.MaxInt32 (~2GB) while
    DefaultGRPCMaxRecvMsgSize was 10MB, a 200x asymmetry with no cap on
    outbound response size. Set both defaults to 10MB so gRPC responses
    can't unboundedly grow server memory out of the box.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -46,6 +46,8 @@ Ref: https://keepachangelog.com/en/1.0.0/
 
 ### Bug Fixes
 
+* (server) [#26585](https://github.com/cosmos/cosmos-sdk/pull/26585) lower default gRPC MaxSendMsgSize from ~2GB to 10MB
+
 ### Deprecated
 
 ## [v0.54.3](https://github.com/cosmos/cosmos-sdk/releases/tag/v0.54.3) - 2026-05-05
--- server/config/config.go
+++ server/config/config.go
@@ -3,7 +3,6 @@ package config
 import (
 	"encoding/json"
 	"fmt"
-	"math"
 
 	"github.com/spf13/viper"
 	"google.golang.org/grpc"
@@ -29,7 +28,7 @@ const (
 
 	// DefaultGRPCMaxSendMsgSize defines the default gRPC max message size in
 	// bytes the server can send.
-	DefaultGRPCMaxSendMsgSize = math.MaxInt32
+	DefaultGRPCMaxSendMsgSize = 1024 * 1024 * 10
 )
 
 // BaseConfig defines the server's basic configuration
@@ -153,7 +152,7 @@ type GRPCConfig struct {
 	MaxRecvMsgSize int `mapstructure:"max-recv-msg-size"`
 
 	// MaxSendMsgSize defines the max message size in bytes the server can send.
-	// The default value is math.MaxInt32.
+	// The default value is 10MB.
 	MaxSendMsgSize int `mapstructure:"max-send-msg-size"`
 
 	// SkipCheckHeader defines if the gRPC server should bypass header checking.
